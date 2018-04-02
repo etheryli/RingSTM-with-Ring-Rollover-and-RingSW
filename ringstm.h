4189b97add697d527e7259dd5c8b594dcc9c8495
Pooja Rathnashyam <poojar3@vt.edu>
2018-04-02 12:35:38 -0400

    added pseudocode

--- ringstm.h
+++ ringstm.h
@@ -46,17 +46,74 @@ struct TX_EXCEPTION {};
 
 class RingSW {
 public:
-  void tx_begin() {}
+  void tx_begin()
+  {
+    /*
+    - Reset thread-local metadata
+    - RV = Global-Clock
+    - while ( ring[RV].status != complete || ring[RV].timestamp < RV)
+    - TX.start--
+    - fence(Read-Before-Read)
+    */
+  }
 
-  int64_t tx_read(int64_t *address) {}
+  int64_t tx_read(int64_t *address)
+  {
+    /*
+  - Find the addr is in the write-set signature
+    • If found, find the addr is in the write-set
+      – return the value buffered in write-set
+  – val = *addr
+  – Add addr to read-set signature
+  – tx_validate()
+  – Return val
+  */
+  }
 
-  void tx_write(int64_t *address, int64_t value);
+  void tx_write(int64_t *address, int64_t value)
+  {
+    /*
+   - Add (or update) the addr and value to the write-set
+   - Add the  addr to the write-set signature
+   */
+  }
 
-  void tx_commit() {}
+  void tx_commit()
+  {
+    /*
+    – if (write-set.size == 0) -> return  //read-only tx
+    – again : commit_time = Global-Clock
+    – tx_validate()
+    – If (!CAS(&Global-Clock, commit_time, commit_time+1))
+      • goto again
+    – ring[commit_time + 1] = {writing, write-set-sig, commit_time +1}
+    – for (i= commit_time downto RV + 1)
+      • if (ring[ ].write-sig ∩ write-set signature)
+        – while(ring[i].status == writing) wait
+    – For each entry in the write-set
+      • *entry.addr = entry.value //Write back
+    – while(ring[commit_time.status == writing) wait
+    – ring[commit_time + 1].status = complete
+    */
+  }
 
-  void tx_validate() {}
+  void tx_validate()
+  {
+    /*
+  - if Global -Clock == RV -> return
+  – end = Global-Clock
+  – while (ring[end].timestamp < end) wait
+  – for ring entries between Global-Clock & RV+1
+    • if (ring-entry.write-sig ∩ read-set signature)
+      – tx_abort ()
+    • if (ring-entry.status  == writing)
+      – end = (ring-entry-index) – 1
+  – RV = end
+  */
+  }
 
   void tx_abort() {
+    ////Just jump back to tx_begin to restart the transaction
     TX_EXCEPTION e;
     throw e;
   }
