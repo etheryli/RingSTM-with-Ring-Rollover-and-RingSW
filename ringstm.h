fcc17c7f72618267e5bee2c5160ec83f33b0ecb7
Pooja Rathnashyam <poojar3@vt.edu>
2018-04-09 09:24:14 -0400

    updated

--- ringstm.h
+++ ringstm.h
@@ -2,7 +2,6 @@
 #define RING_STM_H__
 
 #include "BitFilter.h"
-
 #include "rand_r_32.h"
 #include <fcntl.h>
 #include <pthread.h>
@@ -15,7 +14,7 @@
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
-
+#include <map>
 
 #define CFENCE __asm__ volatile("" ::: "memory")
 #define MFENCE __asm__ volatile("mfence" ::: "memory")
@@ -27,6 +26,7 @@
 
 #define BITS 1024
 
+#define RING_SIZE 8
 
 struct RingEntry {
   uint64_t timestamp = 0;
@@ -35,21 +35,12 @@ struct RingEntry {
   int status = COMPLETED;
 };
 
-// Refactored into class
-struct Transaction {
-  std::set<*uint64_t> wset;
-  BitFilter<BITS> writefilter;
-  BitFilter<BITS> readfilter;
-  uint64_t start;
-};
 
-extern volatile uint64_t global_clock;
-extern RingEntry ring[];
-extern volatile int ring_index;
-extern volatile int prefix_index;
+volatile uint64_t global_clock = 0; // AKA ring_index / the end_index of the ring
+RingEntry ring[RING_SIZE];
+
 struct TX_EXCEPTION {};
-uint64_t* accountsAll;
-#define ACCOUT_NUM 100000
+
 
 class RingSW {
 public:
@@ -62,8 +53,9 @@ public:
     CFENCE;
     // RV = Global-Clock
     RV = global_clock;
+    uint64_t temp = RV % 8;
     // if ( ring[RV].status != complete || ring[RV].timestamp < RV) - >RV--
-    if (ring[RV].status != COMPLETED || ring[RV].timestamp < RV) {
+    if (ring[temp].status != COMPLETED || ring[temp].timestamp < RV) {
       RV--;
     }
     CFENCE;
@@ -96,75 +88,140 @@ public:
      writefilter.add(&address);
   }
 
+  void tx_validate() {
+    /*
+    a rollover test must be issued before returning, to ensure
+    that, after all validation is complete, the oldest entry validated
+    has not been overwritten (accomplished with a test on
+    its timestamp field).This test detects when a validation is
+    interrupted by a context switch, and conservatively aborts
+    if ring rollover occurs while the transaction is swapped out.
+    */
+    CFENCE;
+    uint64_t end = global_clock;
+    CFENCE;
+
+    // Rollover check (2)
+    uint64_t end_index = end % 8;
+    int rollover_check_index = RV % 8;
+    if (ring[rollover_check_index].timestamp != RV) {
+      tx_abort();
+    }
+
+    // greater than expected Check (1)
+    /*if (ring[end_index].timestamp > end) {
+      tx_abort();
+    }*/
+    for (int i = 0; i < RING_SIZE; i++) {
+      if (ring[i].timestamp > end) {
+        tx_abort();
+      }
+    }
+
+    // if Global -Clock == RV -> return
+
+    if(end == RV)
+      return;
+
+    while (ring[end_index].timestamp < end) {
+      //WAIT
+    }
+
+    for(int i = end; i >= RV + 1; i--) {
+      int temp = i % 8;
+      if(ring[temp].wf.intersect(&readfilter)) {
+        tx_abort();
+      }
+    }
+
+    while (ring[end_index].status != COMPLETED) {
+      //WAIT
+    }
+
+    CFENCE;
+    RV = end;
+    CFENCE;
+  }
+
   void tx_commit() {
     // if Read Only -> return
     if(write_set.size() == 0) { return; }
 
-    CFENCE;
   again:
+    CFENCE;
     uint64_t commit_time = global_clock;
+    CFENCE;
+    // (3) Calls to check from tm_end
+    // must block until the newest ring entry’s timestamp is set.
+    uint64_t commit_time_index = commit_time % 8;
+    while (ring[commit_time_index].timestamp != commit_time) {
+      // Block
+    }
+
     tx_validate();
 
     if(!(__sync_bool_compare_and_swap(&global_clock, commit_time, (commit_time+1)))) {
        goto again;
     }
-    /*
-    – ring[commit_time + 1] = {writing, write-set-sig, commit_time +1}
-    – for (i= commit_time downto RV + 1)
-      • if (ring[ ].write-sig ∩ write-set signature)
-        – while(ring[i].status == writing) wait
-    */
-    //For each entry in the write-set
-    for(auto& entry : )
-    {
-      //*entry.addr = entry.value //Write back
 
-    }
-    while(ring[commit_time].status == WRITING);
+    uint64_t new_index = (commit_time+1) % 8;
+
     CFENCE;
-    ring[commit_time + 1].status = COMPLETE;
+    ring[new_index].status = WRITING;
+    ring[new_index].wf = writefilter;
+    ring[new_index].timestamp = commit_time+1;
+    CFENCE;
 
-  }
-
-  void tx_validate()  {
-
-    //if Global -Clock == RV -> return
-    if(global_clock = RV)
-      return;
-
-    int end = global_clock;
-
-    //while (ring[end].timestamp < end) wait
-
-
-    for(int i = RV+1; i < global_clock; i++)
-    {
-      if(ring[i].writefilter.intersect(ring[i].readfilter))
-      {
-          tx_abort();
-      }
-      if (ring[i].status == WRITING)
-      {
-        end = i – 1;
-      }
+    //For each entry in the write-set
+    CFENCE;
+    for(auto &entry : write_set) {
+      *entry.first = entry.second; //Write back
     }
+    CFENCE;
 
-    RV = end;
-
+    CFENCE;
+    ring[new_index].status = COMPLETED;
+    commits++;
+    CFENCE;
   }
+
+
+/*
+1.
+    whenever the expected
+    timestamp of an entry is greater than the value expected
+    by a transaction (either during validate or commit),
+    that transaction must abort.
+
+2.  a rollover test must be issued before returning, to ensure
+    that, after all validation is complete, the oldest entry validated
+    has not been overwritten (accomplished with a test on
+    its timestamp field).This test detects when a validation is
+    interrupted by a context switch, and conservatively aborts
+    if ring rollover occurs while the transaction is swapped out.
+
+3.
+    During initialization, updates to a new ring entry’s timestamp
+    must follow the setting of the write filter and the status
+    (write-after-write ordering). Calls to check from tm_end
+    must block until the newest ring entry’s timestamp is set.
+*/
 
   void tx_abort() {
     ////Just jump back to tx_begin to restart the transaction
+    aborts++;
     TX_EXCEPTION e;
     throw e;
   }
 
+
+  long commits = 0;
+  long aborts = 0;
 private:
-  std::map<int64_t, int64_t> write_set;
+  std::map<int64_t*, int64_t> write_set;
   BitFilter<BITS> writefilter;
   BitFilter<BITS> readfilter;
   uint64_t RV;
-
 };
 
 #endif // RING_STM_H__
