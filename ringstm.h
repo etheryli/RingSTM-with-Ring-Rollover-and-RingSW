d81d714408ede5207d6fb3132a40ab241ebe058c
H u N g <gd0697@gmail.com>
2018-04-09 10:15:06 -0400

    style
    style

--- ringstm.h
+++ ringstm.h
@@ -4,6 +4,7 @@
 #include "BitFilter.h"
 #include "rand_r_32.h"
 #include <fcntl.h>
+#include <map>
 #include <pthread.h>
 #include <setjmp.h>
 #include <stdint.h>
@@ -14,7 +15,6 @@
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
-#include <map>
 
 #define CFENCE __asm__ volatile("" ::: "memory")
 #define MFENCE __asm__ volatile("mfence" ::: "memory")
@@ -35,12 +35,11 @@ struct RingEntry {
   int status = COMPLETED;
 };
 
-
-volatile uint64_t global_clock = 0; // AKA ring_index / the end_index of the ring
+volatile uint64_t global_clock =
+    0; // AKA ring_index / the end_index of the ring
 RingEntry ring[RING_SIZE];
 
 struct TX_EXCEPTION {};
-
 
 class RingSW {
 public:
@@ -62,30 +61,31 @@ public:
   }
 
   int64_t tx_read(int64_t *address) {
-    //Find the addr is in the write-set signature
-    if(writefilter.lookup(address))
-    {
-     //If found, find the addr is in the write-set & return the value buffered in write-set
-     return write_set[address];
+    // Find the addr is in the write-set signature
+    if (writefilter.lookup(address)) {
+      // If found, find the addr is in the write-set & return the value buffered
+      // in write-set
+      return write_set[address];
     }
-    //val = *addr
+    // val = *addr
     CFENCE;
     int64_t val = *address;
     CFENCE;
 
-    //Add addr to read-set signature
+    // Add addr to read-set signature
     readfilter.add(&address);
 
     // Check
     tx_validate();
-    //Return val
+    // Return val
     return val;
   }
 
   void tx_write(int64_t *address, int64_t value) {
-     //Add (or update) the addr and value to the write-set & Add the  addr to the write-set signature
-     write_set[address] = value;
-     writefilter.add(&address);
+    // Add (or update) the addr and value to the write-set & Add the  addr to
+    // the write-set signature
+    write_set[address] = value;
+    writefilter.add(&address);
   }
 
   void tx_validate() {
@@ -115,25 +115,25 @@ public:
 
     // if Global -Clock == RV -> return
 
-    if(end == RV)
+    if (end == RV)
       return;
 
     while (ring[end_index].timestamp < end) {
-      //WAIT
+      // WAIT
     }
 
-    for(int i = end; i >= RV + 1; i--) {
+    for (int i = end; i >= RV + 1; i--) {
       int temp = i % 8;
-      if(ring[temp].wf.intersect(&readfilter)) {
+      if (ring[temp].wf.intersect(&readfilter)) {
         tx_abort();
       }
     }
 
     while (ring[end_index].status != COMPLETED) {
-      //WAIT
+      // WAIT
     }
 
-      // Rollover test (2)
+    // Rollover test (2)
     uint64_t rollover_check_index = RV % 8;
     if (ring[rollover_check_index].timestamp != RV) {
       tx_abort();
@@ -142,12 +142,13 @@ public:
     CFENCE;
     RV = end;
     CFENCE;
-
   }
 
   void tx_commit() {
     // if Read Only -> return
-    if(write_set.size() == 0) { return; }
+    if (write_set.size() == 0) {
+      return;
+    }
 
   again:
     CFENCE;
@@ -162,22 +163,23 @@ public:
 
     tx_validate();
 
-    if(!(__sync_bool_compare_and_swap(&global_clock, commit_time, (commit_time+1)))) {
-       goto again;
+    if (!(__sync_bool_compare_and_swap(&global_clock, commit_time,
+                                       (commit_time + 1)))) {
+      goto again;
     }
 
-    uint64_t new_index = (commit_time+1) % 8;
+    uint64_t new_index = (commit_time + 1) % 8;
 
     CFENCE;
     ring[new_index].status = WRITING;
     ring[new_index].wf = writefilter;
-    ring[new_index].timestamp = commit_time+1;
+    ring[new_index].timestamp = commit_time + 1;
     CFENCE;
 
-    //For each entry in the write-set
+    // For each entry in the write-set
     CFENCE;
-    for(auto &entry : write_set) {
-      *entry.first = entry.second; //Write back
+    for (auto &entry : write_set) {
+      *entry.first = entry.second; // Write back
     }
     CFENCE;
 
@@ -187,27 +189,26 @@ public:
     CFENCE;
   }
 
+  /*
+  1.
+      whenever the expected
+      timestamp of an entry is greater than the value expected
+      by a transaction (either during validate or commit),
+      that transaction must abort.
 
-/*
-1.
-    whenever the expected
-    timestamp of an entry is greater than the value expected
-    by a transaction (either during validate or commit),
-    that transaction must abort.
+  2.  a rollover test must be issued before returning, to ensure
+      that, after all validation is complete, the oldest entry validated
+      has not been overwritten (accomplished with a test on
+      its timestamp field).This test detects when a validation is
+      interrupted by a context switch, and conservatively aborts
+      if ring rollover occurs while the transaction is swapped out.
 
-2.  a rollover test must be issued before returning, to ensure
-    that, after all validation is complete, the oldest entry validated
-    has not been overwritten (accomplished with a test on
-    its timestamp field).This test detects when a validation is
-    interrupted by a context switch, and conservatively aborts
-    if ring rollover occurs while the transaction is swapped out.
-
-3.
-    During initialization, updates to a new ring entry’s timestamp
-    must follow the setting of the write filter and the status
-    (write-after-write ordering). Calls to check from tm_end
-    must block until the newest ring entry’s timestamp is set.
-*/
+  3.
+      During initialization, updates to a new ring entry’s timestamp
+      must follow the setting of the write filter and the status
+      (write-after-write ordering). Calls to check from tm_end
+      must block until the newest ring entry’s timestamp is set.
+  */
 
   void tx_abort() {
     ////Just jump back to tx_begin to restart the transaction
@@ -216,11 +217,11 @@ public:
     throw e;
   }
 
-
   long commits = 0;
   long aborts = 0;
+
 private:
-  std::map<int64_t*, int64_t> write_set;
+  std::map<int64_t *, int64_t> write_set;
   BitFilter<BITS> writefilter;
   BitFilter<BITS> readfilter;
   uint64_t RV;
